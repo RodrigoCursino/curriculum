d91fbce8d3eb79046d64a0ae3dc642aa19e7a0d9
RodrigoCursino <rodrigoapmc@gmail.com>
2018-03-27 17:54:19 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <curriculum></curriculum>
+  </div>
+</template>
+
+<script>
+import Curriculum from "./components/Curriculum";
+
+export default {
+  components: {Curriculum},
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Curriculum.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="d-flex align-items-stretch">
+      <div class="p-2 back-blue"><i class="fab fa-github icon"></i></div>
+      <div class="p-2 back-green"></div>
+      <div class="p-2 back-red"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Curriculum',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  $red: rgb(255.0,0.0,0.0);
+  $green: rgb(0.0,255.0,0.0);
+  $blue: rgb(0.0,0.0,255.0);
+  .icon {
+    color: white;
+    font-size: 1.8em;
+  }
+  .back-red {
+    background-color: $red;
+  };
+  .back-blue {
+    background-color: $blue;
+  };
+  .back-green {
+    background-color: $green;
+  };
+</style>
